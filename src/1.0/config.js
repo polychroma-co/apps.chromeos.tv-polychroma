764e20797f30871bd37a31d74278a82f6009b92c
Polychroma, LLC <support@polychroma.co>
2017-10-11 14:28:33 -0400

    Add files via upload

---
+++ src/1.0/config.js
@@ -0,0 +1,12 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "TV Polychroma",
+   "version": "1.0",
+   "homepage": "http://polychroma.co",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": false
+};
